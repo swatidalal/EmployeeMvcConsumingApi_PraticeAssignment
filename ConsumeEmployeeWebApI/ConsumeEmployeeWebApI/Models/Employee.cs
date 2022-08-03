72e670cd50bb82a72b6935bb39037448a19ac4a8
swatidalal <swati15091999@gmail.com>
2022-08-03 09:46:27 +0530

    initial Commit

---
+++ ConsumeEmployeeWebApI/ConsumeEmployeeWebApI/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsumeEmployeeWebApI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }       
+        public string? Mobile { get; set; }
+        public string? Email { get; set; }
+        public string? Address_1 { get; set; }
+        public string? Address_2{ get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+
+
+
+    }
+}
---
+++ EmployeesApp_29July/Employees.Api/Data/EmployeeApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Employees.Api.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.Api.Data
+{
+    public class EmployeeApiDbContext:DbContext
+    {
+        public DbSet<Employee> Employee { set; get; }
+
+
+        public EmployeeApiDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Employee>().HasIndex(Emp => Emp.Email).IsUnique();
+
+
+        }
+    }
+}
